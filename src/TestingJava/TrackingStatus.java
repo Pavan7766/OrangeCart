1926703d52a91971a47f3ff7707a0da2b9d42c37
hp <hp@DESKTOP-D9LVMSH>
2025-06-01 13:32:18 +0530

    User Branch code commit

---
+++ src/TestingJava/TrackingStatus.java
@@ -0,0 +1,11 @@
+package TestingJava;
+
+public class TrackingStatus {
+
+	public static void main(String[] args) {
+		System.out.println("Please find the Tracking details");
+		System.out.println("From Bangalore to Mysore Distance");
+
+	}
+
+}
