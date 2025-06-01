3bdd7454b83822e7cff86fe5c01cdb7a10aaf68b
LENOVO <LENOVO@LAPTOP-PD5J7D6R>
2025-06-01 13:17:37 +0530

    code changed as per the comments

--- src/TestingJava/BhavishyaCode.java
+++ src/TestingJava/BhavishyaCode.java
@@ -3,8 +3,8 @@ package TestingJava;
 public class BhavishyaCode {
 
 	public static void main(String[] args) {
-	System.out.println("Welcome to Bajpe ");
-
+	System.out.println("Welcome to Bajpe");
+	System.out.println("Welcome to sunkadakatte");
 	}
 
 }
